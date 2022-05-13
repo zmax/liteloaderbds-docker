fec7c82ebcbf7192baefc9eadbf58f60f29464c5
Starck Lin <zenyer@gmail.com>
2022-05-13 12:26:24 +0800

    update build-data.sh

--- build-data.sh
+++ build-data.sh
@@ -9,7 +9,6 @@ wget https://minecraft.azureedge.net/bin-win/bedrock-server-${BDSVER}.zip && \
 wget https://github.com/LiteLDev/LiteLoaderBDS/releases/download/${LLVER}/LiteLoader-${LLVER}.zip && \
 unzip -n bedrock-server-${BDSVER}.zip -d ${BDSDIR} && \
 unzip -n LiteLoader-${LLVER}.zip -d ${BDSDIR} && \
-cp ./vcruntime140_1.dll ${BDSDIR} && \
-cp ./bedrock_server_mod.exe ${BDSDIR} && \
+unzip -n requirements.zip -d ${BDSDIR} && \
 rm ./bedrock-server-${BDSVER}.zip && \
 rm ./LiteLoader-${LLVER}.zip
